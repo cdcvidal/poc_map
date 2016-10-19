161bb1c97c4944f4565422c522f1418ad0026bd6
cdcvidal <celine_vidal@natural-solutions.eu>
2016-10-19 21:32:10 +0200

    Remove comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -40,35 +40,6 @@ export class MyApp {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
-
-      // BackgroundGeolocation is highly configurable. See platform specific configuration options
-      let config = {
-              desiredAccuracy: 10,
-              stationaryRadius: 20,
-              distanceFilter: 30,
-              debug: true, //  enable this hear sounds for background-geolocation life-cycle.
-              stopOnTerminate: false, // enable this to clear background location settings when the app terminates
-      };
-
-      BackgroundGeolocation.configure((location) => {
-        console.log('[js] BackgroundGeolocation callback:  ' + location.latitude + ',' + location.longitude);
-
-         // IMPORTANT:  You must execute the finish method here to inform the native plugin that you're finished,
-         // and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
-         // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
-         BackgroundGeolocation.finish(); // FOR IOS ONLY
-
-      }, (error) => {
-          console.log('BackgroundGeolocation error');
-      }, config);
-
-     // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
-     BackgroundGeolocation.start();
-
-     //INIT IGN
-     
-
-
     });
   }
 
